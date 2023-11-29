7a8480d1980692311d816b6d3dcc952ce695ba5c
mask-shakill <mask.shakil@gmail.com>
2023-11-29 18:59:17 +0600

    function added

---
+++ intermediate/function.ts
@@ -0,0 +1,15 @@
+// Simple function declaration
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+// Simple function usage
+const sum = add(3, 4);
+console.log(sum); // Output: 7
+
+// Arrow function
+const multiply = (a: number, b: number): number => a * b;
+
+// Arrow function usage
+const product = multiply(5, 6);
+console.log(product); // Output: 30
